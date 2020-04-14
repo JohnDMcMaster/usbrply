4a4e985e1cef0b663c607da87c6c1ff31d212e2d
John McMaster <johndmcmaster@gmail.com>
2020-04-14 11:03:40 -0700

    hexdump: import from uvscada
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- usbrply/util.py
+++ usbrply/util.py
@@ -1,3 +1,49 @@
+import sys
+
+
+def hexdump(data, label=None, indent='', address_width=8, f=sys.stdout):
+    def isprint(c):
+        return c >= ' ' and c <= '~'
+
+    if label:
+        print label
+
+    bytes_per_half_row = 8
+    bytes_per_row = 16
+    data = bytearray(data)
+    data_len = len(data)
+
+    def hexdump_half_row(start):
+        left = max(data_len - start, 0)
+
+        real_data = min(bytes_per_half_row, left)
+
+        f.write(''.join('%02X ' % c for c in data[start:start + real_data]))
+        f.write(''.join('   ' * (bytes_per_half_row - real_data)))
+        f.write(' ')
+
+        return start + bytes_per_half_row
+
+    pos = 0
+    while pos < data_len:
+        row_start = pos
+        f.write(indent)
+        if address_width:
+            f.write(('%%0%dX  ' % address_width) % pos)
+        pos = hexdump_half_row(pos)
+        pos = hexdump_half_row(pos)
+        f.write("|")
+        # Char view
+        left = data_len - row_start
+        real_data = min(bytes_per_row, left)
+
+        f.write(''.join([
+            c if isprint(c) else '.'
+            for c in str(data[row_start:row_start + real_data])
+        ]))
+        f.write((" " * (bytes_per_row - real_data)) + "|\n")
+
+
 def add_bool_arg(parser, yes_arg, default=False, **kwargs):
     dashed = yes_arg.replace('--', '')
     dest = dashed.replace('-', '_')
