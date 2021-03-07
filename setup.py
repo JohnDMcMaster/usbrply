6b17fec49f093e31a4449f293f5524727155e8bc
John McMaster <johndmcmaster@gmail.com>
2021-03-07 14:14:32 -0800

    setup.py: v2.1.0
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ for src_fn, prog_name in scripts:
 
 setup(
     name="usbrply",
-    version="2.0.1",
+    version="2.1.0",
     author="John McMaster",
     author_email='JohnDMcMaster@gmail.com',
     description=("Replay captured USB packets from .pcap file."),
