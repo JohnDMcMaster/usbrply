ab2f612c0f846a36c42b1d6d724ae5526ea523ae
John McMaster <johndmcmaster@gmail.com>
2022-03-13 00:45:52 -0800

    linux control tests
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- test/test_misc.py
+++ test/test_misc.py
@@ -27,36 +27,53 @@ class TestCase(unittest.TestCase):
 
     def test_parse_win_pcap(self):
         """Windows .pcap parse test"""
-        parsers.jgen2j(usbrply.parsers.pcap2json("test/data/win1.pcapng", argsj=self.argsj))
+        parsers.jgen2j(
+            usbrply.parsers.pcap2json("test/data/win1.pcapng",
+                                      argsj=self.argsj))
 
     def test_parse_lin_pcap(self):
         """Linux .pcap parse test"""
-        parsers.jgen2j(usbrply.parsers.pcap2json("test/data/lin1.pcapng", argsj=self.argsj))
+        parsers.jgen2j(
+            usbrply.parsers.pcap2json("test/data/lin1.pcapng",
+                                      argsj=self.argsj))
 
     def test1(self):
         return
-        j = parsers.jgen2j(usbrply.parsers.pcap2json("test/data/win1.pcapng", argsj=self.argsj))
+        j = parsers.jgen2j(
+            usbrply.parsers.pcap2json("test/data/win1.pcapng",
+                                      argsj=self.argsj))
         for d in j["data"]:
             if d["type"] == "controlRead":
                 print(d)
 
     def test_print_json(self):
-        usbrply.printers.run(
-            "json", usbrply.parsers.pcap2json("test/data/lin1.pcapng", argsj=self.argsj), argsj=self.argsj)
+        usbrply.printers.run("json",
+                             usbrply.parsers.pcap2json("test/data/lin1.pcapng",
+                                                       argsj=self.argsj),
+                             argsj=self.argsj)
 
     def test_print_pyprinter_lin(self):
-        usbrply.printers.run(
-            "libusb-py", usbrply.parsers.pcap2json("test/data/lin1.pcapng", argsj=self.argsj), argsj=self.argsj)
+        usbrply.printers.run("libusb-py",
+                             usbrply.parsers.pcap2json("test/data/lin1.pcapng",
+                                                       argsj=self.argsj),
+                             argsj=self.argsj)
 
     def test_print_pyprinter_win(self):
-        usbrply.printers.run(
-            "libusb-py", usbrply.parsers.pcap2json("test/data/win1.pcapng", argsj=self.argsj), argsj=self.argsj)
+        usbrply.printers.run("libusb-py",
+                             usbrply.parsers.pcap2json("test/data/win1.pcapng",
+                                                       argsj=self.argsj),
+                             argsj=self.argsj)
+
+    """
+    Windows
+    """
 
     def test_win_interrupt(self):
-        usbrply.printers.run(
-            "json",
-            usbrply.parsers.pcap2json("test/data/win_interrupt.pcapng", argsj=self.argsj), argsj=self.argsj)
-
+        usbrply.printers.run("json",
+                             usbrply.parsers.pcap2json(
+                                 "test/data/win_interrupt.pcapng",
+                                 argsj=self.argsj),
+                             argsj=self.argsj)
 
     def test_win_pipes(self):
         """
@@ -67,34 +84,65 @@ class TestCase(unittest.TestCase):
         Not exactly sure what this is but its at the beginning of my test capture
         Normally there?
         """
-        usbrply.printers.run(
-            "json",
-            usbrply.parsers.pcap2json("test/data/win_setup_pipes.pcapng", argsj=self.argsj), argsj=self.argsj)
+        usbrply.printers.run("json",
+                             usbrply.parsers.pcap2json(
+                                 "test/data/win_setup_pipes.pcapng",
+                                 argsj=self.argsj),
+                             argsj=self.argsj)
 
     def test_win_bulk_out(self):
         """
         Verify bulk out parses on Windows
         """
-        usbrply.printers.run(
-            "json",
-            usbrply.parsers.pcap2json("test/data/win_setup_bulk-out.pcapng", argsj=self.argsj), argsj=self.argsj)
+        usbrply.printers.run("json",
+                             usbrply.parsers.pcap2json(
+                                 "test/data/win_setup_bulk-out.pcapng",
+                                 argsj=self.argsj),
+                             argsj=self.argsj)
 
-    def test_win_bulk_in(self):
+    def test_win_control_in(self):
         """
-        Verify bulk in parses on Windows
+        Verify control in parses on Windows
         """
-        usbrply.printers.run(
-            "json",
-            usbrply.parsers.pcap2json("test/data/win_setup_control-in.pcapng", argsj=self.argsj), argsj=self.argsj)
+        usbrply.printers.run("json",
+                             usbrply.parsers.pcap2json(
+                                 "test/data/win_setup_control-in.pcapng",
+                                 argsj=self.argsj),
+                             argsj=self.argsj)
 
     def test_win_control_out(self):
         """
         Verify control out parses on Windows
         """
-        usbrply.printers.run(
-            "json",
-            usbrply.parsers.pcap2json("test/data/win_setup_control-out.pcapng", argsj=self.argsj), argsj=self.argsj)
+        usbrply.printers.run("json",
+                             usbrply.parsers.pcap2json(
+                                 "test/data/win_setup_control-out.pcapng",
+                                 argsj=self.argsj),
+                             argsj=self.argsj)
 
+    """
+    Linux
+    """
+
+    def test_lin_control_in(self):
+        """
+        Verify control in parses on Linux
+        """
+        usbrply.printers.run("json",
+                             usbrply.parsers.pcap2json(
+                                 "test/data/lin_setup_control-in.pcapng",
+                                 argsj=self.argsj),
+                             argsj=self.argsj)
+
+    def test_lin_control_out(self):
+        """
+        Verify control out parses on Linux
+        """
+        usbrply.printers.run("json",
+                             usbrply.parsers.pcap2json(
+                                 "test/data/lin_setup_control-out.pcapng",
+                                 argsj=self.argsj),
+                             argsj=self.argsj)
 
 
 if __name__ == "__main__":
